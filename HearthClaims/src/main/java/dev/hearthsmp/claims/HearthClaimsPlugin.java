1bae1b5d6968dde78867b93126f11e66eade6301
yazoniplay <yazan.mahmoud2@outlook.com>
2026-09-24 18:41:39 +0200

    Build HearthClaims land protection system

---
+++ HearthClaims/src/main/java/dev/hearthsmp/claims/HearthClaimsPlugin.java
@@ -0,0 +1,40 @@
+package dev.hearthsmp.claims;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HearthClaimsPlugin extends JavaPlugin {
+    private ClaimManager claimManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        claimManager = new ClaimManager(this);
+        claimManager.load();
+
+        getServer().getPluginManager().registerEvents(new ClaimListener(this), this);
+
+        ClaimCommand command = new ClaimCommand(this);
+        getCommand("claim").setExecutor(command);
+        getCommand("claim").setTabCompleter(command);
+
+        TrustCommand trust = new TrustCommand(this);
+        getCommand("trust").setExecutor(trust);
+        getCommand("untrust").setExecutor(trust);
+
+        getCommand("abandonclaim").setExecutor(new AbandonClaimCommand(this));
+
+        getLogger().info("HearthClaims is protecting the Hearth.");
+    }
+
+    @Override
+    public void onDisable() {
+        if (claimManager != null) {
+            claimManager.save();
+        }
+    }
+
+    public ClaimManager getClaimManager() {
+        return claimManager;
+    }
+}
