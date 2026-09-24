e205e54ea1486e727972b717cfc71e97e82dd26a
yazoniplay <yazan.mahmoud2@outlook.com>
2026-09-24 18:37:58 +0200

    Build HearthCore foundation

--- HearthCore/src/main/java/dev/hearthsmp/core/HearthCorePlugin.java
+++ HearthCore/src/main/java/dev/hearthsmp/core/HearthCorePlugin.java
@@ -1,15 +1,51 @@
 package dev.hearthsmp.core;
 
+import dev.hearthsmp.core.command.HearthCommand;
+import dev.hearthsmp.core.config.HearthConfig;
+import dev.hearthsmp.core.data.PlayerDataService;
+import dev.hearthsmp.core.ui.HearthUI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HearthCorePlugin extends JavaPlugin {
+    private HearthConfig hearthConfig;
+    private PlayerDataService playerDataService;
+    private HearthUI hearthUI;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        hearthConfig = new HearthConfig(this);
+        playerDataService = new PlayerDataService(this);
+        hearthUI = new HearthUI(this);
+
+        hearthConfig.load();
+        playerDataService.start();
+
+        getCommand("hearth").setExecutor(new HearthCommand(this));
+        getCommand("hearth").setTabCompleter(new HearthCommand(this));
+
         getLogger().info("HearthCore is warming the Hearth.");
+        getLogger().info("UI, configuration, and player-data services are ready.");
     }
 
     @Override
     public void onDisable() {
+        if (playerDataService != null) {
+            playerDataService.shutdown();
+        }
         getLogger().info("HearthCore is shutting down.");
+    }
+
+    public HearthConfig getHearthConfig() {
+        return hearthConfig;
+    }
+
+    public PlayerDataService getPlayerDataService() {
+        return playerDataService;
+    }
+
+    public HearthUI getHearthUI() {
+        return hearthUI;
     }
 }
